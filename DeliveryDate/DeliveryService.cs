56e4e020240371486dfaaa78a11549592e62eded
Daria Burtseva <qmemorais@gmail.com>
2023-05-17 15:00:17 +0200

    Initial commit

---
+++ DeliveryDate/DeliveryService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace DeliveryDate
+{
+    internal class DeliveryService
+    {
+        readonly TimeSpan openHour = new(8, 0, 0);
+        readonly TimeSpan closeHour = new(17, 0, 0);
+        readonly TimeSpan midnight = new(24, 0, 0);
+        readonly int deliveryTimeBetweenPoints = 2;
+        readonly int checkDocumantationTime = 1;
+
+        public DateTime FindExpectedDeliveryDate(DateTime orderedDate)
+        {
+            if (!IsWorkingHours(orderedDate))
+                orderedDate = WaitForWorkingHours(orderedDate);
+            
+            var expectedDeliveryTime = DeliverPackageOrWaitDocumentation(orderedDate, deliveryTimeBetweenPoints);
+
+            expectedDeliveryTime = DeliverPackageOrWaitDocumentation(expectedDeliveryTime, checkDocumantationTime);
+
+            expectedDeliveryTime = DeliverPackageOrWaitDocumentation(expectedDeliveryTime, deliveryTimeBetweenPoints);
+
+            return expectedDeliveryTime;
+        }
+
+        private DateTime DeliverPackageOrWaitDocumentation(DateTime date, int timeToWait)
+        {
+            var expectedDateToDeliver = date.AddHours(timeToWait);
+
+            while (!IsWorkingHours(expectedDateToDeliver))
+            {
+                expectedDateToDeliver = WaitForWorkingHours(expectedDateToDeliver);
+                expectedDateToDeliver = expectedDateToDeliver.AddHours(timeToWait);
+            }
+            return expectedDateToDeliver;
+        }
+
+        private DateTime WaitForWorkingHours(DateTime date)
+        {
+            var orderedTimeBeforOpenHour = openHour - date.TimeOfDay;
+            var orderedTimeAfterCloseHour = date.TimeOfDay - closeHour;
+
+            if (orderedTimeBeforOpenHour.TotalMinutes > 0)
+                date = date.Add(orderedTimeBeforOpenHour);
+
+            if (orderedTimeAfterCloseHour.TotalMinutes > 0 
+                || date.DayOfWeek == DayOfWeek.Sunday 
+                || date.DayOfWeek == DayOfWeek.Saturday)
+                date = date.Add(midnight - date.TimeOfDay + openHour);
+
+            if (!IsWorkingHours(date))
+                date = WaitForWorkingHours(date);
+
+            return date;
+        }
+
+        private bool IsWorkingHours(DateTime orderedDate)
+            => orderedDate.DayOfWeek != DayOfWeek.Sunday && orderedDate.DayOfWeek != DayOfWeek.Saturday
+                && orderedDate.TimeOfDay >= openHour && orderedDate.TimeOfDay <= closeHour;
+    }
+}
---
+++ DeliveryDate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DeliveryDate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //to input dd.mm.yyyy hh:mm instead of mm.dd.yyyy hh:mm
+            System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.CreateSpecificCulture("en-gb");
+
+            Console.WriteLine("Please input date and time of order in format dd.MM.yyyy HH:mm");
+
+            var isDateParsed = DateTime.TryParse(Console.ReadLine(), out var inputedDateTime);
+
+            if (isDateParsed)
+            {
+                DeliveryService deliver = new();
+
+                var expectedDeliveryDate = deliver.FindExpectedDeliveryDate(inputedDateTime);
+
+                Console.WriteLine($"Expected Delivery Date: {expectedDeliveryDate}");
+            }
+
+            Console.WriteLine("Enter any button");
+            Console.ReadKey();
+        }
+    }
+}
